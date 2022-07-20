bca6088d0330f764cc1bed2c4030c13b99d658f0
CWebber <cbwebber@hotmail.com>
2022-07-20 16:36:58 +1000

    Created functions to calculate, as well as initialised variables for num1 and num2.

--- Calculator.js
+++ Calculator.js
@@ -1,1 +1,18 @@
+let num1 = 0;
+let num2 = 0;
 
+function Add(num1, num2) {
+    return num1 + num2;
+}
+
+function Subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function Multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function Divide(num1, num2) {
+    return num1 / num2;
+}
