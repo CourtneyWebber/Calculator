ac8c600475b7bb6ed249cd2a5f12beb100dfa597
CWebber <cbwebber@hotmail.com>
2022-07-21 14:32:48 +1000

    calculation and display working

--- Calculator.js
+++ Calculator.js
@@ -1,6 +1,3 @@
-let num1 = 0;
-let num2 = 0;
-
 function Add(num1, num2) {
     return num1 + num2;
 }
@@ -15,4 +12,28 @@ function Multiply(num1, num2) {
 
 function Divide(num1, num2) {
     return num1 / num2;
+}
+
+function GetCalculate() {
+    let num1 = Number(document.getElementById("num1").value);
+    let num2 = Number(document.getElementById("num2").value);
+    let operator = document.querySelector('input[name="operator"]:checked').value;    
+
+    switch (operator) {
+        case "+":
+            document.getElementById("result").innerText = Add(num1, num2);
+            break;
+        case "-":
+            document.getElementById("result").innerText = Subtract(num1, num2);
+            break;
+        case "*":
+            document.getElementById("result").innerText = Multiply(num1, num2);
+            break;
+        case "/":
+            document.getElementById("result").innerText = Divide(num1, num2);
+            break;
+        default:
+            return "Try again";
+            break;
+    }
 }
